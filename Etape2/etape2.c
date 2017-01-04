990e52963778d049e0ae7e3c4ccbeb1dbd06ba18
Sauww <bastien.zigmann@free.fr>
2017-01-04 16:41:23 +0100

    Ajouter nom

--- Etape2/etape2.c
+++ Etape2/etape2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <string.h>
 
 //<=> readelf -S file1.o
 
@@ -32,11 +33,12 @@ int main(int argc, char *argv[]){
     	exit(1);
   	}
     
+    //on se place au début de la table des headers
   	fseek(fichierElf,structElf32.e_shoff,0); 
    	
    	//structElf32.e_shentsize <=> taille de la table des sections
    	
-  	//printf("Numéro||Nom||Type||Addr||Décal||Taille||Flags||Link||Info||Al\n");
+  	printf("N||Nom    ||Type    ||Addr    ||Décal ||Taille||ES||Fl||L||I||A\n");
 
   	for(i=0;i<structElf32.e_shnum;i++){
   	
@@ -45,15 +47,122 @@ int main(int argc, char *argv[]){
     		printf("failed to read elf section header\n");
     		exit(1);
   		}
-
-  		//printf("%i||",i);
+		
+		//Numéro
+  		printf("%i||",i);
   		
-  		//printf("%s||",lire_nom(structElf32,structSectionHeader, fichierElf));
-  		lire_nom(structElf32,structSectionHeader, fichierElf);
-  		printf("\n");
+  		//nom
+  		printf("NOM_ICI||");//,lire_nom(structElf32,structSectionHeader, fichierElf));
+  		
+  		//Type
+  		switch(structSectionHeader.sh_type){
   			
+  			case SHT_NULL:
+  				printf("NULL    ");
+  				break;
+  			case SHT_PROGBITS:
+  				printf("PROGBITS");
+  				break;
+  			case SHT_SYMTAB:
+  				printf("SYMTAB  ");
+  				break;
+  			case SHT_STRTAB:
+  				printf("STRTAB  ");
+  				break;
+  			case SHT_RELA:
+  				printf("RELA    ");
+  				break;
+  			case SHT_HASH:
+  				printf("HASH    ");
+  				break;
+  			case SHT_DYNAMIC:
+  				printf("DYNAMIC ");
+  				break;
+  			case SHT_NOTE:
+  				printf("NOTE    ");
+  				break;
+  			case SHT_NOBITS:
+  				printf("NOBITS  ");
+  				break;
+  			case SHT_REL:
+  				printf("REL     ");
+  				break;
+  			case SHT_SHLIB:
+  				printf("SHLIB   ");
+  				break;
+  			case SHT_DYNSYM:
+  				printf("DYNSYM  ");
+  				break;
+  			case SHT_LOPROC:
+  				printf("LOPROC  ");
+  				break;
+  			case SHT_HIPROC:
+  				printf("HIPROC  ");
+  				break;
+  			case SHT_NUM:
+  				printf("NUM     ");
+  				break;
+  			case SHT_LOUSER:
+  				printf("LOUSER  ");
+  				break;
+  			case SHT_HIUSER:
+  				printf("HIUSER  ");
+  			default:
+  				printf("Others  ");
+  				break;
+  			
+  		}
+  		printf("||");
+  		//adresse
+  		printf("%8.8x||",structSectionHeader.sh_addr);
+  		//décalage
+  		printf("%6.6x||",structSectionHeader.sh_offset);
+  		//taille
+  		printf("%6.6x||",structSectionHeader.sh_size);
+  		//ES
+  		printf("%2.2x||",structSectionHeader.sh_entsize);
+  		//Flags
+  		char* r = malloc(sizeof(char)*12);
+  		if (structSectionHeader.sh_flags & 0x1)
+  			r = strcat(r,"W");
+  		if (structSectionHeader.sh_flags & 0x2)
+  			r = strcat(r,"A");
+  		if (structSectionHeader.sh_flags & 0x4)
+  			r = strcat(r,"X");
+  		if (structSectionHeader.sh_flags & 0x10)
+  			r = strcat(r,"M");
+  		if (structSectionHeader.sh_flags & 0x20)
+  			r = strcat(r,"S");
+  		if (structSectionHeader.sh_flags & 0x40)
+  			r = strcat(r,"I");
+  		if (structSectionHeader.sh_flags & 0x80)
+  			r = strcat(r,"L");
+  		if (structSectionHeader.sh_flags & 0x100)
+  			r = strcat(r,"O");
+  		if (structSectionHeader.sh_flags & 0x200)
+  			r = strcat(r,"G");
+  		if (structSectionHeader.sh_flags & 0x400)
+  			r = strcat(r,"T");
+  		if (structSectionHeader.sh_flags & 0x0ff00000)
+  			r = strcat(r,"O");
+  		if (structSectionHeader.sh_flags & 0xf0000000)
+  			r = strcat(r,"P");
+  		if(strlen(r)==0)
+  			r = strcat(r,"  ");
+  		printf("%s||",r);
+  		
+  		//Link
+  		printf("%i||",structSectionHeader.sh_link);
+  		//Info
+  		printf("%i||",structSectionHeader.sh_info);
+  		//Allignement
+  		printf("%i",structSectionHeader.sh_addralign);
+
+  	
+  	printf("\n");
+  	
   	}
-   	
+   	printf("\nN = Numéro, Fl = Flags, L = Link, I = Info, A = Allignement\n");
 	return 0;
 	
 }
@@ -61,17 +170,17 @@ int main(int argc, char *argv[]){
 char* lire_nom(Elf32_Ehdr structElf32, Elf32_Shdr structSectionHeader, FILE* fich){
 	
 	char* nom;
-	char c;
+	char c='a';
 	int i = 0;
 	long pos = ftell(fich);
-	
-	fseek(fich,structElf32.e_shstrndx + structSectionHeader.sh_name,0); 
+
+	fseek(fich,structElf32.e_shstrndx + structSectionHeader.sh_name,SEEK_SET); 
 	
 	while(c!='\0'){
 
-		fread(&c,1,1,fich);
+		fread(&c,1,sizeof(char),fich);
 		i++;
-		printf("%c\n",c+'0');
+		printf("%c\n",c);
 		//nom[i]=c;
 		
 	}
