3e6ee7307ed0b1f9d6877468db9149f3f3b039b5
thomasduverney <thomas.duverney@etu.univ-grenoble-alpes.com>
2017-01-05 13:59:19 +0100

    Etape3 Terminée

--- Etape3/etape3.c
+++ Etape3/etape3.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <elf.h>
 #include <string.h>
+#include <ctype.h>
 #include "fonctionUtile.h"
 
-
+void fonctionEtape3(FILE * fichierElf,char * section,Elf32_Ehdr structElf32, Elf32_Shdr * tabHeaders);
 
 int main(int argc, char *argv[]){
 	
 	FILE* fichierElf = NULL;
     
 	if(argc != 2){
-    	printf("Syntaxe : ./etape2 file\n");
+    	printf("Syntaxe : ./etape3 file\n");
     	exit(1);
      }
     
@@ -26,9 +27,12 @@ int main(int argc, char *argv[]){
     Elf32_Ehdr structElf32 = lireHeaderElf(argv);
         
 
-   Elf32_Shdr * tabHeaders = accesTableDesHeaders(structElf32,fichierElf);
-
-/*	int j;
+    Elf32_Shdr * tabHeaders = accesTableDesHeaders(structElf32,fichierElf);
+	fonctionEtape3(fichierElf,"5",structElf32,tabHeaders);
+	
+	
+	/*
+	int j;
 	for(j=0;j<structElf32.e_shnum; j++){
 	
 		printf("%s \n",lire_nom(structElf32,j,fichierElf));
@@ -36,14 +40,26 @@ int main(int argc, char *argv[]){
 		printf("%i \n",tabHeaders[j].sh_offset);
 	}*/
 	
-	
-	
-	Elf32_Shdr tempHed = RechercheSectionByName(fichierElf,".shstrtab",tabHeaders,structElf32);
-
-	afficheSection(tempHed.sh_offset,tempHed.sh_size,fichierElf);
-	
 	return 0;
 	
 }
+
+void fonctionEtape3(FILE * fichierElf,char * section,Elf32_Ehdr structElf32, Elf32_Shdr * tabHeaders){
+	Elf32_Shdr tempHed;
+	printf("Affichage de la section : %s \n",section);
+	
+	if(isalpha(*section) != 0){
+	
+		tempHed = RechercheSectionByName(fichierElf,section,tabHeaders,structElf32);
+		afficheSection(tempHed.sh_offset,tempHed.sh_size,fichierElf);
+	
+	}else{
+		
+		Elf32_Shdr tempHed = tabHeaders[atoi(section)];
+		afficheSection(tempHed.sh_offset,tempHed.sh_size,fichierElf);
+		
+	}
+	
+}  
 
 
