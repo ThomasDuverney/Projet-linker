932fba01691ec203424bfe34b4dace6fcbb03f4f
Guillaume Cailhe <cailheg@F212-08.e-im2ag.ujf-grenoble.fr>
2017-01-09 16:34:02 +0100

    Factorisation de myreadElf, possibilité de mettre plusieurs arguments, ajout du -a

--- readElf/myReadElf.c
+++ readElf/myReadElf.c
@@ -1,111 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <getopt.h>
 #include <string.h>
 #include "fonctionUtile.h"
 #include "phase1.h"
 
+void afficherParametres(){
+			printf("Utilisation de myReadElf : ./myReadElf -option ...\n");
+			printf("Options : -h <fichier> \t\t\t\t\t Affiche les infos contenu dans le header du fichier elf en paramètre\n");
+			printf("\t -a <fichier> \t\t\t\t\t  Affiche toutes les informations du fichier elf passé en paramètre\n");
+			printf("\t  -S <fichier> \t\t\t\t\t Affiche les informations de la table des sections du fichier elf passé en paramètre\n");
+			printf("\t  -x <numéro (ou nom) de section> <fichier> \t Affiche le contenu de la section de numéro (ou nom) passé en pramaètre\n");
+			printf("\t  -s <fichier> \t\t\t\t\t Affiche la table des symboles du fichier elf passé en paramètre\n");
+			printf("\t  -r <fichier> \t\t\t\t\t Affichage des réallocations contenu dans le fichier elf passé en paramètre\n");
+}
 
 int main (int argc, char *argv[]){
 
-  FILE* fichierElf;
-  Elf32_Ehdr structElf32;
+  	FILE* fichierElf;
+	Elf32_Ehdr structElf32;
   
-  if (argc>=3 && argv[1][0] == '-' && strlen(argv[1]) == 2){
-	  
-	    switch(argv[1][1]){
-		
-	    case 'h':
-	    
-	      if(argc == 3){
-	      
-	      	 fichierElf = ouvrirFichier(argv[2]);
-	      	 structElf32 = lireHeaderElf(fichierElf);
-	      	 fonctionEtape1(structElf32);
-	     	 fclose(fichierElf);
-	      }else{
-	   
-	      	 printf(" Essayez myReadElf -h \"nomFichier\"\n"); 
-	   
-	      }
-	 
-	      break;
-	    case 'S':
-	    	if(argc == 3){
-	    	  
-	    	   fichierElf = ouvrirFichier(argv[2]);
-	      	   structElf32 = lireHeaderElf(fichierElf);
-	           fonctionEtape2(structElf32,fichierElf);
-		   	   fclose(fichierElf);
-			}else{
+	int opt;
+	static int do_header, do_sections, 
+				do_hex_dump, do_symbols, 
+				do_relocs; // flags pour les options
 
-	      	 	printf(" Essayez myReadElf -S \"nomFichier\"\n"); 
+	// Tableau des options
+	struct option longopts[] = {
+		{ "all", no_argument, 0, 'a' },
+		{ "header", no_argument, 0, 'h' },
+		{ "sections", no_argument, 0, 'S'},
+		{ "hex-dump", required_argument, 0, 'x'},
+		{ "symbols", no_argument, 0, 's'},
+		{ "relocs",	no_argument, 0, 'r'},
+		{ NULL, 0, NULL, 0 }
+	};
 
-	      	}
-		  		   
-	      break;
-	    case 'x':
-	    	
-	    	if(argc == 4){
-	    		
-	    		fichierElf = ouvrirFichier(argv[3]);
-	      	 	structElf32 = lireHeaderElf(fichierElf);
-	      	 	fonctionEtape3(fichierElf,argv[2],structElf32);
-	      	 	fclose(fichierElf);
-	    		
-	    	}else{
-	    		
-	    		printf("Essayez myReadElf -x \"numéroDeSection\" \"nomFichier\"\n");
-	    		
-	    	}
-	      
-	      break;
-	    case 's':
-	      
-	      if(argc == 3){
-	      	
-	      	    fichierElf = ouvrirFichier(argv[2]);
-	      	 	structElf32 = lireHeaderElf(fichierElf);
-	      	 	fonctionEtape4(fichierElf,structElf32);
-	      	
-	      }else{
-	      	
-	      	printf("Essayez myReadElf -s \"nomFichier\"\n");
-	      	
-	      }
-	      
-	      break;
-	    case 'r':
-	      
-	      if(argc == 3){
-	      
-	      	fichierElf = ouvrirFichier(argv[2]);
-	      	structElf32 = lireHeaderElf(fichierElf);
-	      	fonctionEtape5(structElf32,fichierElf);
-	      	fclose(fichierElf);
-	      	
-	      }else{
-	      	
-	      	printf("Essayez myReadElf -r \"nomFichier\"\n");	
-	      	
-	      }
-	      
-	      break;
-	    default:
-	      printf("Ne correspond à aucun type\n");
-	      break;
-	      
-	  }
+	// Parcours des options
+	if(argc >= 3){
+		char * sectionHexDump;
+		while ((opt = getopt_long(argc, argv, "ahSsrx:", longopts, NULL)) != -1) {	
+			switch(opt) {
+				case 'a':
+					do_header++;
+					do_sections++;
+					do_symbols++;
+					do_relocs++;
+					break;
+				case 'h':
+					do_header++;
+					break;
+				case 'S':
+					do_sections++;
+					break;
+				case 'x':
+					do_hex_dump++;
+					sectionHexDump = optarg;
+					break;
+				case 's':
+					do_symbols++;
+					break;
+				case 'r':
+					do_relocs++;
+					break;
+				default:
+					fprintf(stderr, "Option inconnue %c\n", opt);
+					afficherParametres();
+					exit(1);
+			}
+		}
 
-  }else{
-  		printf("Utilisation de myReadElf : ./readElf -option ...\n");
-  		printf("Options : -h <fichier> \t\t\t\t\t Affiche les infos contenu dans le header du fichier elf en paramètre\n");
-  		printf("\t  -S <fichier> \t\t\t\t\t Affiche les informations de la table des sections du fichier elf passé en paramètre\n");
-  		printf("\t  -x <numéro (ou nom) de section> <fichier> \t Affiche le contenu de la section de numéro (ou nom) passé en pramaètre\n");
-  		printf("\t  -s <fichier> \t\t\t\t\t Affiche la table des symboles du fichier elf passé en paramètre\n");
-  		printf("\t  -r <fichier> \t\t\t\t\t Affichage des réallocations contenu dans le fichier elf passé en paramètre\n");
-  }
+		// Ouverture du fichier
+		fichierElf = ouvrirFichier(argv[optind++]);
+		structElf32 = lireHeaderElf(fichierElf);
 
-  return 0;
+		// Appel des fonctions nécessaires
+		if(do_header){
+			fonctionEtape1(structElf32);
+		}
+	
+		if(do_sections){
+			fonctionEtape2(structElf32,fichierElf);
+		}	
 
+		if(do_hex_dump){
+			printf("Section : %s\n", sectionHexDump);
+			fonctionEtape3(fichierElf,sectionHexDump, structElf32);
+		}	
+
+		if(do_symbols){
+			fonctionEtape4(fichierElf,structElf32);
+		}	
+
+		if(do_relocs){
+			fonctionEtape5(structElf32,fichierElf);
+		}
+	
+		// Fermeture du fichier
+		fclose(fichierElf);
+	}else{
+		afficherParametres();
+	}
+
+  	return 0;
 }
