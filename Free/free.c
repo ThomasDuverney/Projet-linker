217a4ab930f15c113d01cd6796dbd37313bd871c
Sauww <bastien.zigmann@free.fr>
2017-01-11 10:10:37 +0100

    Free factorisé yakoi

--- Free/free.c
+++ Free/free.c
@@ -4,7 +4,8 @@
 #include <elf.h>
 #include <string.h>
 
-void LibererMemoire(ContenuFus * contenuFus,Elf32_Shdr * TabHeaders);
+void libererMemoire(ContenuFus * contenuFus,Elf32_Shdr * TabHeaders);
+void libererContenuElf(ContenuElf * contenuElf);
 
 int main (int argc, char *argv[]){
 
@@ -16,7 +17,7 @@ int main (int argc, char *argv[]){
 	fichierElf = ouvrirFichier(argv[1]);
 	remplirStructure(fichierElf,contenuFus->contenuElf1,&TabHeaders);
 
-	LibererMemoire(contenuFus,TabHeaders);	
+	libererMemoire(contenuFus,TabHeaders);	
 
 		// Fermeture du fichier
 		fclose(fichierElf);
@@ -24,59 +25,44 @@ int main (int argc, char *argv[]){
 return 0;
 }
 
-void LibererMemoire(ContenuFus * contenuFus,Elf32_Shdr * TabHeaders){
+void libererMemoire(ContenuFus * contenuFus,Elf32_Shdr * TabHeaders){
 
-int i;
 	if(contenuFus->contenuElf1 != NULL){
-		free(contenuFus->contenuElf1->tableString);
-		free(contenuFus->contenuElf1->TableNomSection);
-		free(contenuFus->contenuElf1->tabSymb);
-		free(contenuFus->contenuElf1->tabRela);
-		free(TabHeaders);
-		printf("%i\n",contenuFus->contenuElf1->hdrElf.e_shnum);
 		
-		for (i=0;i<contenuFus->contenuElf1->hdrElf.e_shnum;i++){
-			free((contenuFus->contenuElf1->tabSections)[i].contenuSection);
-		}	
-		free(contenuFus->contenuElf1->tabSections);		
-		free(contenuFus->contenuElf1);
-		
+		libererContenuElf(contenuFus->contenuElf1);
 		
 	}
 	
 	if(contenuFus->contenuElf2 != NULL){
 		
-		free(contenuFus->contenuElf2->tableString);
-		free(contenuFus->contenuElf2->TableNomSection);
-		free(contenuFus->contenuElf2->tabSymb);
-		free(contenuFus->contenuElf2->tabRela);
-		free(TabHeaders);
-		printf("%i\n",contenuFus->contenuElf2->hdrElf.e_shnum);
-		
-		for (i=0;i<contenuFus->contenuElf2->hdrElf.e_shnum;i++){
-			free((contenuFus->contenuElf2->tabSections)[i].contenuSection);
-		}	
-		free(contenuFus->contenuElf2->tabSections);		
-		free(contenuFus->contenuElf2);
+		libererContenuElf(contenuFus->contenuElf2);
 		
 		
 	}
 	
 	if(contenuFus->contenuElfFinal != NULL){
 		
-		free(contenuFus->contenuElfFinal->tableString);
-		free(contenuFus->contenuElfFinal->TableNomSection);
-		free(contenuFus->contenuElfFinal->tabSymb);
-		free(contenuFus->contenuElfFinal->tabRela);
-		free(TabHeaders);
-		printf("%i\n",contenuFus->contenuElfFinal->hdrElf.e_shnum);
-		
-		for (i=0;i<contenuFus->contenuElfFinal->hdrElf.e_shnum;i++){
-			free((contenuFus->contenuElfFinal->tabSections)[i].contenuSection);
-		}	
-		free(contenuFus->contenuElfFinal->tabSections);		
-		free(contenuFus->contenuElfFinal);
+		libererContenuElf(contenuFus->contenuElfFinal);
 	
 	}
-free(contenuFus);
+	free(TabHeaders);
+	free(contenuFus);
+}
+
+void libererContenuElf(ContenuElf * contenuElf){
+
+	int i;
+	
+	free(contenuElf->tableString);
+	free(contenuElf->TableNomSection);
+	free(contenuElf->tabSymb);
+	free(contenuElf->tabRela);
+	//printf("%i\n",contenuElfFinal->hdrElf.e_shnum);
+	
+	for (i=0;i<contenuElf->hdrElf.e_shnum;i++){
+		free((contenuElf->tabSections)[i].contenuSection);
+	}	
+	free(contenuElf->tabSections);		
+	free(contenuElf);
+	
 }
